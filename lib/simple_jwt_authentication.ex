936b33729c2de95689bbba95b0c6809d17a9c524
Trevor Fenn <sgtpepper43@gmail.com>
2016-10-28 16:38:05 -0600

    Add ability to check and set required fields

--- lib/simple_jwt_authentication.ex
+++ lib/simple_jwt_authentication.ex
@@ -6,32 +6,55 @@ defmodule SimpleJWTAuthentication do
   A plug that checks for presence of a simple token for authentication
   """
   @behaviour Plug
-  def init(opts), do: opts
+  def init(opts \\ []), do: opts
 
-  def call(conn, _opts) do
-    conn
-    |> get_jwt
-    |> jwt_is_valid?
-    |> if do
-        conn
-      else
-        conn
-        |> put_resp_content_type("application/json")
-        |> send_resp(401, ~s({ "error": "Invalid token" }))
-        |> halt
-      end
+  def call(conn, opts) do
+    case validate_token(conn, opts) do
+      {:ok, conn} -> conn
+      {:error, error} -> send_error(conn, error)
+    end
   end
 
-  defp jwt_is_valid?(nil), do: false
-  defp jwt_is_valid?(jwt), do: jwt_is_valid?(jwt, Application.get_env(:simple_jwt_authentication, :secret_key))
+  defp validate_token(conn, opts) do
+    conn
+    |> get_jwt
+    |> parse_jwt
+    |> case do
+      {:ok, jwt} -> assign_required_fields(conn, jwt, opts)
+      error -> error
+    end
+  end
 
-  defp jwt_is_valid?(_, nil), do: false
-  defp jwt_is_valid?(jwt, secret_key) do
+  defp send_error(conn, error) do
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(401, ~s({ "error": #{error} }))
+    |> halt
+  end
+
+  defp assign_required_fields(conn, _, []), do: {:ok, conn}
+  defp assign_required_fields(conn, jwt, fields) do
+    Enum.reduce(fields, {:ok, conn}, fn
+      {key, path}, {:ok, conn} ->
+        case get_in(jwt.fields, path) do
+          nil -> {:error, "Missing field: #{inspect key}"}
+          value -> {:ok, assign(conn, key, value)}
+        end
+      _, {:error, error} -> {:error, error}
+    end)
+  end
+
+  defp parse_jwt(nil), do: {:error, "No token"}
+  defp parse_jwt(jwt), do: parse_jwt(jwt, Application.get_env(:simple_jwt_authentication, :secret_key))
+
+  defp parse_jwt(_, nil), do: {:error, "No secret key configured"}
+  defp parse_jwt(jwt, secret_key) do
     jwk = %{"kty" => "oct", "k" => secret_key}
 
     case JWT.verify(jwk, jwt) do
-      {true, token, _} -> !expired?(token)
-      _ -> false
+      {true, jwt, _} ->
+        if expired?(jwt), do: {:error, "Token has expired"}, else: {:ok, jwt}
+      _ -> {:error, "Invalid token"}
     end
   end
 
